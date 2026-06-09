56c2971d360e81fbb5ff6408525f1f07402cc93a
easygoing22 <psujinkr@gmail.com>
2026-06-09 16:38:01 +1200

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>내 첫 React 앱</h1>
+      <p>민서민채 사랑해</p>
+    </div>
+  );
+}
+
+export default App;
